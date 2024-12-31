7350fd344846127f40a1f94f9a24c0d3ddd3d920
jagat-evince <152966655+jagat-evince@users.noreply.github.com>
2024-12-31 08:02:19 +0000

    basics

--- basics.js
+++ basics.js
@@ -1,1 +1,32 @@
-console.log("Jagat");
+// Type of 
+let a = 111
+let b = "11"
+let c = null
+let d = true
+let f = 12344555666456564n
+let g = function () {}
+// console.log(typeof a, typeof b, typeof c, typeof d, typeof e, typeof f, typeof g)
+
+
+// Conversion 
+let isLoggein = 0
+let booleanConv = Boolean(isLoggein)
+// console.log(booleanConv);
+// console.log(typeof booleanConv);
+
+
+// Operation 
+let aa = 100
+++aa
+
+let bb = 100
+bb++
+
+// console.log(aa + bb);
+
+
+// Comparision 
+
+console.log("2" === 2);
+
+
